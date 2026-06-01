652bd0bf12af89ee9aa526bd3fe33be7fa474654
Valen Perdamenta <tinusv17@gmail.com>
2026-06-01 09:36:11 +0700

    fix bug be

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use("/api/histori", require("./routes/historiRoutes"));
 app.use("/api/jadwal", require("./routes/jadwalRoutes"));
 app.use("/api/optimasi", require("./routes/optimasiRoutes"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
